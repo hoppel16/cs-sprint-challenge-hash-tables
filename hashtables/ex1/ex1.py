65d7f43b8622976ff246058f118fe91785828fe6
Hunter Oppel <hun.oppel@gmail.com>
2020-08-07 12:20:55 -0400

    Finished ex1, brute force

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,14 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    for ind1 in range(0, length):
+        if ind1 + 1 >= length:
+            break
+        for ind2 in range(ind1+1, length):
+            if weights[ind1] + weights[ind2] is limit:
+                return ind2, ind1
 
     return None
+
+
+if __name__ == "__main__":
+    weights = [1, 3, 5, 7, 12, 16]
+    print(get_indices_of_item_weights(weights, len(weights), 15))
