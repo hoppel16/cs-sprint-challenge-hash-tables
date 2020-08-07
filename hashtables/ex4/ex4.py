3743e45c463f9acfbaab5409140d39cba020a63d
Hunter Oppel <hun.oppel@gmail.com>
2020-08-07 11:55:08 -0400

    Finished ex4. Finished MVP

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,17 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    result = []
+    number_cache = {}
+
+    for number in a:
+        if number not in number_cache:
+            number_cache[number] = 0
+        number_cache[number] += 1
+
+        if number * -1 in number_cache and abs(number) not in result:
+            if number is 0:
+                if number_cache[number] < 2:
+                    continue
+            result.append(abs(number))
 
     return result
 
